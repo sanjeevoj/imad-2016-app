4acdaf44c8859ec7f8180528e4476cef32e163bb
sanjeevoj <sanjeev.oj@gmail.com>
2016-09-19 14:41:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ function createTemplate(data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
-    var content = dta.content;
-    var htmtemplate = `<html>
+    var content = data.content;
+    var htmltemplate = `<html>
         <head>
             <title>
-            ${title}   
+                 ${title}   
             </title>
             <meta name="viewport" content="width-device-width, initial-scale-1"/>
             <link href="/ui/style.css" rel="stylesheet" />
@@ -52,7 +52,7 @@ function createTemplate(data) {
             </body>
     </html>
     `;
-    return htmtemplate;
+    return htmltemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
